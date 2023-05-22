250796d64c80fc9c1cf9285f13945acf0b138261
Kaaamil <126326972+Kaaaanapka@users.noreply.github.com>
2023-05-22 15:32:59 +0200

    Update imped2.py

--- imped2.py
+++ imped2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sounddevice as sd
 from scipy.fftpack import fft
+import matplotlib.pyplot as plt
 
 def generate_signal(frequency, duration, sampling_rate):
     t = np.linspace(0, duration, int(duration * sampling_rate), endpoint=False)
@@ -18,9 +19,30 @@ def perform_fft(signal):
 def calculate_amplitude_ratio(input_signal, output_signal):
     return np.divide(output_signal, input_signal)
 
+def plot_signal(signal, sampling_rate, title):
+    duration = len(signal / sampling_rate)
+    time = np.linspace(0, duration, len(signal))
+    plt.figure()
+    plt.plot(time, signal)
+    plt.title(title)
+    plt.xlabel("Czas (s)")
+    plt.ylabel("Amplituda")
+    plt.show()
+
+def plot_fft (fft_result, sampling_rate, title):
+    duration = len(fft_result) / sampling_rate
+    freq = np.linspace(0, 1 / ( 2 *duration), len (fft_result) // 2 )
+    plt.figure()
+    plt.plot(freq, np.abs(fft_result[:len(fft_result) //2]))
+    plt.title(title)
+    plt.xlabel("Czestotliwosc (Hz)")
+    plt.ylabel("Amplituda")
+    plt.show()
+
 def main():
     # Parametry sygnału
-    frequencies = [1000, 2000, 5000]  # Przykładowe częstotliwości
+    frequencies = [100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 11000, 12000, 13000,
+                    14000, 15000, 16000, 17000, 18000, 19000, 20000]  # Przykładowe częstotliwości
     duration = 1.0
     sampling_rate = 44100
 
@@ -41,5 +63,13 @@ def main():
         print(f"Amplituda FFT wyjścia / FFT wejścia dla częstotliwości {frequency} Hz:")
         print(amplitude_ratio)
 
+
+
+        plot_signal(signal, sampling_rate,  f"Teoretyczny sygnal - {frequency} Hz")
+        plot_signal(recorded_signal, sampling_rate, f"Nagrany sygnal - {frequency} Hz")
+        plot_signal(amplitude_ratio, sampling_rate,  f"Amplituda FFT wyjścia / FFT wejścia dla częstotliwości - {frequency} Hz")
+        plot_fft(fft_input, sampling_rate, f"FFT wejscie - {frequency} Hz")
+        plot_fft(fft_output, sampling_rate, f"FFT wyjscie - {frequency} Hz")
+        
 if __name__ == "__main__":
     main()
